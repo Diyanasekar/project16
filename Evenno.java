34bd93336052079125b687af5cd1c5fe7729d719
Diyanasekar <diyanas1997@gmail.com>
2017-07-05 11:07:40 +0530

    Create Evenno.java

---
+++ Evenno.java
@@ -0,0 +1,25 @@
+# project16
+import java.io.*;
+import java.util.*;
+public class Evenno
+{
+  
+  public static void main(String args[])
+  {
+    
+    Scanner sc=new Scanner(System.in);
+    int n;
+    n=sc.nextInt();
+    for(int i=1;i<n;i++)
+    {
+      if(i%2==0)
+      {
+        
+        System.out.println(i);
+      }
+    }
+  }
+}
+
+
+     
